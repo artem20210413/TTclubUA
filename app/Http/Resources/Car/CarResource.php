54899983c936d0480a8c96af957a62d40fe258de
Artem Tishchenko <tishchenko@MacBook-Air-mac.local>
2025-04-06 23:32:47 +0300

    fixed updated_at

--- app/Http/Resources/Car/CarResource.php
+++ app/Http/Resources/Car/CarResource.php
@@ -36,7 +36,7 @@ class CarResource extends JsonResource
             'model' => new ModelResource($this->model),
             'imageUrls' => $imageUrls,
             'active' => $this->active,
-            'updated_at' => $this->updated_at->diffForHumans(),
+            'updated_at' => $this->updated_at?->diffForHumans(),
             'created_at' => $this->created_at?->diffForHumans(),
         ];
     }

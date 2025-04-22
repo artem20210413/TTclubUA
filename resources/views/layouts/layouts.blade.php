0781cfb7948a867a213b723bd1c4e0a250ddf946
Artem_PC <artemwbtv@gmail.com>
2025-04-22 21:58:02 +0300

    delete build

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -6,7 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    @vite(['resources/css/app.css', 'resources/css/bootstrap/bootstrap.min.css', 'resources/js/app.js'])
+{{--    @vite(['resources/css/app.css', 'resources/css/bootstrap/bootstrap.min.css', 'resources/js/app.js'])--}}
+    <link rel="stylesheet" href="{{ asset('build/assets/app-DdHod6mR.css') }}">
+    <script src="{{ asset('build/assets/app-eMHK6VFw.js') }}" defer></script>
+
     <title>@yield('title')</title>
 
     <style>

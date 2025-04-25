eacb4f2a9cc0d7aba722ad9a21e94dd4efff5561
Artem Tishchenko <tishchenko@MacBook-Air-mac.local>
2025-04-25 15:09:11 +0300

    added OnlyFromUkraine

---
+++ app/Http/Middleware/OnlyFromUkraine.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Stevebauman\Location\Facades\Location;
+
+class OnlyFromUkraine
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $ip = $request->ip();
+
+        // Можно учитывать Cloudflare или прокси
+        if ($request->header('CF-Connecting-IP')) {
+            $ip = $request->header('CF-Connecting-IP');
+        }
+
+        $position = Location::get($ip);
+        dump($ip, $position);
+        if (app()->environment('local')) {
+            return $next($request);
+        }
+        if ($position && $position->countryCode === 'UA') {
+            return $next($request);
+        }
+
+        // Можно вернуть кастомное сообщение
+        abort(403, 'Доступ дозволено лише з України 🇺🇦');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Route;
 //    return view('welcome');
 //});
 
-Route::get('/', [\App\Http\Controllers\Web\RegistrationsController::class, 'index'])->name('web.home');
+Route::get('/', [\App\Http\Controllers\Web\RegistrationsController::class, 'index'])->middleware('only.ua')->name('web.home');
 Route::post('/registration', [\App\Http\Controllers\Web\RegistrationsController::class, 'registration'])->name('web.post.registration');
 

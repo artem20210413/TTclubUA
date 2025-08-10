29d82cfb88ae818e8f6f0918bd6fc096b575765b
Artem_PC <artemwbtv@gmail.com>
2025-08-10 16:44:53 +0300

    added event and delete publication

--- app/Enum/EnumTypeMedia.php
+++ app/Enum/EnumTypeMedia.php
@@ -7,7 +7,6 @@ enum EnumTypeMedia: string
     case PROFILE_PICTURE = 'profile_picture';
     case PHOTO_COLLECTION = 'photo_collection';
     case PHOTO_MENTION = 'photo_mention';
-    case PHOTO_PUBLICATION = 'photo_publication';
     case PHOTO_EVENT = 'photo_event';
     case PHOTO_EVENT_TYPE = 'photo_event_type';
 

94e2c365f60004a0d1017bcc95f2ddf06b07b7ee
Artem_PC <artemwbtv@gmail.com>
2025-09-28 12:55:21 +0300

    new welcome

--- resources/views/welcome/welcome.blade.php
+++ resources/views/welcome/welcome.blade.php
@@ -15,9 +15,13 @@
             background-repeat: no-repeat;
             height: 100vh;
             width: 100%;
+            display: flex;
         }
 
         .gradient {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
             width: 75vw;
             height: 100vh;
             flex-shrink: 0;
@@ -42,7 +46,7 @@
         }
 
         button.go {
-
+cursor: pointer;
             width: 348px;
             height: 87px;
             padding: 10px;
@@ -58,6 +62,12 @@
             box-shadow: 0 0 10px 0 rgba(189, 235, 255, 0.49);
         }
 
+        button.go :hover {
+            border: 2px solid #000;
+            background: radial-gradient(219.21% 50% at 50% 50%, #FFF 0%, rgba(255, 255, 255, 0.35) 100%);
+            box-shadow: 0 0 10px 0 rgba(189, 235, 255, 0.49);
+        }
+
     </style>
 
 
@@ -65,36 +75,53 @@
     <main class="banner">
         <div class="gradient">
 
-            <div style="display: flex;">
-                <div style="display: flex; align-items: flex-start; width: 648px; flex-direction: column; gap: 20px;">
-                    <img class="logo" src="{{asset('media/images/logo.webp')}}">
-                    <p class="header">Вітаємо! У спільноті фанатів Audi TT</p>
-                    <p class="basis">Нас об'єднує стиль, динаміка і любов до легендарної Audi TT. Атмосфера, підтримка,
-                        спільні
-                        поїздки та
-                        справжні знайомства. Спільнота — об’єднати власників і фанатів Audi TT для спілкування,
-                        підтримки та
-                        обміну досвідом. Події — організація зустрічей, поїздок і заходів для учасників клубу.</p>
-                    <button class="go">
-                        Хочу до вас!
-                    </button>
-                </div>
-            </div>
+            <div style="display: flex;
+            flex-direction: column;
+            justify-content: center;
+            margin-left: 5vw;
+            gap: 60px;">
 
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="83" height="83" viewBox="0 0 83 83" fill="none">
-                    <circle cx="41.5" cy="41.5" r="41.5" fill="#303234"/>
-                    <circle cx="41.5" cy="41.5001" r="32.7389" fill="#26292D"/>
-                    <path d="M49.45 38.6714C51.45 39.8261 51.45 42.7129 49.45 43.8676L39.4292 49.6531C37.4292 50.8078 34.9292 49.3644 34.9292 47.055L34.9292 35.484C34.9292 33.1745 37.4292 31.7312 39.4292 32.8859L49.45 38.6714Z" fill="white"/>
-                </svg>
-{{--                <svg xmlns="http://www.w3.org/2000/svg" width="83" height="83" viewBox="0 0 83 83" fill="none">--}}
-{{--                    <circle cx="41.5" cy="41.5" r="40.5" fill="#303234" stroke="white" stroke-width="2"/>--}}
-{{--                    <circle cx="41.5" cy="41.5001" r="32.7389" fill="#26292D"/>--}}
-{{--                    <path d="M49.45 38.6714C51.45 39.8261 51.45 42.7129 49.45 43.8676L39.4292 49.6531C37.4292 50.8078 34.9292 49.3644 34.9292 47.055L34.9292 35.484C34.9292 33.1745 37.4292 31.7312 39.4292 32.8859L49.45 38.6714Z" fill="white"/>--}}
-{{--                </svg>--}}
-                <div>
-
+                <div style="display: flex;">
+                    <div
+                        style="display: flex; align-items: flex-start; width: 648px; flex-direction: column; gap: 20px;">
+                        <img class="logo" src="{{asset('media/images/logo.webp')}}">
+                        <p class="header">Вітаємо! У спільноті фанатів Audi TT</p>
+                        <p class="basis">Нас об'єднує стиль, динаміка і любов до легендарної Audi TT. Атмосфера,
+                            підтримка,
+                            спільні
+                            поїздки та
+                            справжні знайомства. Спільнота — об’єднати власників і фанатів Audi TT для спілкування,
+                            підтримки та
+                            обміну досвідом. Події — організація зустрічей, поїздок і заходів для учасників клубу.</p>
+                        <button class="go">
+                            Хочу до вас!
+                        </button>
+                    </div>
                 </div>
+
+                <div style="display: flex;
+    align-items: center;
+    flex-direction: row;
+    gap: 22px;;
+            ">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="83" height="83" viewBox="0 0 83 83" fill="none">
+                        <circle cx="41.5" cy="41.5" r="41.5" fill="#303234"/>
+                        <circle cx="41.5" cy="41.5001" r="32.7389" fill="#26292D"/>
+                        <path
+                            d="M49.45 38.6714C51.45 39.8261 51.45 42.7129 49.45 43.8676L39.4292 49.6531C37.4292 50.8078 34.9292 49.3644 34.9292 47.055L34.9292 35.484C34.9292 33.1745 37.4292 31.7312 39.4292 32.8859L49.45 38.6714Z"
+                            fill="white"/>
+                    </svg>
+                    <p>Дивись запрошення від голови клубу</p>
+                    {{--                <svg xmlns="http://www.w3.org/2000/svg" width="83" height="83" viewBox="0 0 83 83" fill="none">--}}
+                    {{--                    <circle cx="41.5" cy="41.5" r="40.5" fill="#303234" stroke="white" stroke-width="2"/>--}}
+                    {{--                    <circle cx="41.5" cy="41.5001" r="32.7389" fill="#26292D"/>--}}
+                    {{--                    <path d="M49.45 38.6714C51.45 39.8261 51.45 42.7129 49.45 43.8676L39.4292 49.6531C37.4292 50.8078 34.9292 49.3644 34.9292 47.055L34.9292 35.484C34.9292 33.1745 37.4292 31.7312 39.4292 32.8859L49.45 38.6714Z" fill="white"/>--}}
+                    {{--                </svg>--}}
+                    <div>
+
+                    </div>
+                </div>
+
             </div>
 
         </div>

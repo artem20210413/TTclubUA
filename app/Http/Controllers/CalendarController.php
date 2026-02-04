6d629ebb09a574fdc4005ebf7229188ca5a99023
Artem_PC <artemwbtv@gmail.com>
2026-02-04 21:11:19 +0200

    added calendarDescription

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -129,7 +129,7 @@ class CalendarController extends Controller
             if ($code === 'event') {
                 $event = Event::find($id);
                 if (!$event) throw new ApiException('Подію не знайдено', 0, 404);
-                $message = "{title}\n\nДата: {date}\nМісце: {place}\nКарта:{map}\n\n{description}"; //\n\nДетальніше: {url}
+                $message = "{title}\n\n{description}\n\nДата: {date}\nМісце: {place}\nКарта:{map}"; //\n\nДетальніше: {url}
                 $url = route('events.show_public', ['event' => $event->id]);
 
                 $message = str_replace('{title}', $event->title, $message);

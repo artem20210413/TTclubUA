4e69427e1029918f4dd969ce7966e4f08a9d90dd
Artem_PC <artemwbtv@gmail.com>
2026-05-10 18:01:59 +0300

    added NotificationsPush

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -30,24 +30,24 @@ class TestController extends Controller
     public function test(Request $request)
     {
 
-        $user = User::first();
-
-        if (!$user) return "Користувачів не знайдено!";
-
-        // 2. Створюємо сповіщення
-        // Це автоматично закине джобу SendPushNotificationJob у чергу
-        $notification = Notification::create([
-            'user_id' => $user->id,
-            'title'   => 'Тестовий заїзд! 🏎',
-            'body'    => 'Це перевірка черги та Firebase. Фа-фа!',
-            'type'    => NotificationsPushType::FA_FA,
-            'data'    => [
-                'car_id' => 1,
-                'test_mode' => true
-            ],
-        ]);
-
-        return "Сповіщення збережено в БД (ID: {$notification->id}). Чекаємо на обробку черги...";
+//        $user = User::first();
+//
+//        if (!$user) return "Користувачів не знайдено!";
+//
+//        // 2. Створюємо сповіщення
+//        // Це автоматично закине джобу SendPushNotificationJob у чергу
+//        $notification = Notification::create([
+//            'user_id' => $user->id,
+//            'title'   => 'Тестовий заїзд! 🏎',
+//            'body'    => 'Це перевірка черги та Firebase. Фа-фа!',
+//            'type'    => NotificationsPushType::FA_FA,
+//            'data'    => [
+//                'car_id' => 1,
+//                'test_mode' => true
+//            ],
+//        ]);
+//
+//        return "Сповіщення збережено в БД (ID: {$notification->id}). Чекаємо на обробку черги...";
 //        Fcm::sendPush('fw_omk1kQICdKUCgOPlm29:APA91bEgeMrA3FQ084NPkW0hxpg92KSQCWrVESR_kkgoPNJffcNh7pZFRVyKWCXd66Ym6AZqzwQ73OviSTqV7PYBPkhaj8OhOPcPHv_RUXMvwE88E16XYVE');
 //        $h = new SendingStatisticsMention();
 //        $h->handle();
